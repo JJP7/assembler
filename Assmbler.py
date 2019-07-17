6d483ebd315acd7e464ff7a4a53795efd87aee61
JJP7 <palimajuver@gmail.com>
2019-07-17 18:16:26 +0800

    Refactored, Extracted methods, Replaced temp with query

--- Assmbler.py
+++ Assmbler.py
@@ -5,16 +5,6 @@ compdict = {"0": "0101010", "1": "0111111", "-1": "0111010", "D": "0001100", "A"
             "M+1": "1110111", "D-1": "0001110", "A-1": "0110010", "M-1": "1110010", "D+A": "0000010", "D+M": "1000010", "D-A": "0010011", "D-M": "1010011", "A-D": "0000111", "M-D": "1000111", "D&A": "0000000", "D&M": "1000000", "D|A": "0010101", "D|M": "1010101"}
 jumpdict = {"null": "000", "JGT": "001", "JEQ": "010", "JGE": "011", "JLT": "100", "JNE": "101", "JLE": "110", "JMP": "111"}
 symboldict = {"R0": "0", "R1": "1", "R2": "2", "R3": "3", "R4": "4", "R5": "5", "R6": "6", "R7": "7", "R8": "8", "R9": "9", "R10": "10", "R11": "11", "R12": "12", "R13": "13", "R14": "14", "R15": "15", "SCREEN": "16384", "KBD": "24576", "SP": "0", "LCL": "1", "ARG": "2", "THIS": "3", "THAT": "4"}
-instructiondict = {}
-
-
-class HackAssembler:
-
-    pass
-
-class Tests:    # should we do this?
-
-    pass
 
 def string_test(toTest):   # do something like for test in unit test check test
 
@@ -25,20 +15,6 @@ def string_test(toTest):   # do something like for test in unit test check test
         passtest = True
 
     return passtest
-
-# All I'm doing is just a copy paste here nothing fancy...
-
-def get_machine_code():
-    pass
-
-def parse_instruction(instrcuction):
-    pass
-
-def get_instruction():      # gets the instruction
-
-    instruction = ""
-
-    return instruction
 
 # Fuck comments, the code should speak for itself!
 
@@ -64,22 +40,42 @@ def convert2binary16(decimal):
     else:
         print("decimal is not a string!")
 
+def isNoise(line): 
 
-# def get_next_instruction(next_instruction):
-#
-#     next_instruction = next_instruction
-#
-#     return next_instruction
-
-
-def translate():
-    pass
-
+    if (isComment(line) == True) or (line[0] == " ") or (line == ""):
+        return True
+        
+    else
+        return False
+           
+def isInstruction(line):
+    
+    if (isNoise(line) == False) and (isLabel(line) == False):
+        return True
+        
+    else
+        return False
+        
+def getLabel(line):
+    
+    label = ""
+    
+    for c in line.strip():
+        
+        if c == ")":
+            break
+            
+        elif c is not "(":
+            	label += c
+            	
+     return label
+        
+        
 # should have followed the proposed API I suppose
 
 def numberfile():
 
-    # okay, we're gonna store it in a dictionary, I think.
+    global symboldict
 
     f = open(r"C:\Users\JUVER\Downloads\nand2tetris\nand2tetris\projects\06\pong\Pong.asm", "r")
 
@@ -88,54 +84,20 @@ def numberfile():
     linenum = 0
     instruction_num = -1
 
-
     for line in f:
 
-        loadlabel = False
-        addLabel = False
-        temp = ""
-        isInstruction = False
-
-        #check if it is whitespace, comments or empty lines else it is an instruction
-        # should be like parse_line() or something
-        for c in line.strip():
-
-            #I'll just deal with them the simple way lol, can't be bothered
-
-
-            # I need to handle in-line comments
-
-            if c == "/":    # this is in binary mode though
-                break
-
-            elif c == " ":
-                break
-
-            elif c == "":
-                break
-
-            elif c == "(":    # this actually isn't complete but meh.
-
-                loadlabel = True
-
-            elif c == ")":
-                loadlabel = False
-                addLabel = True
-
-            elif loadlabel == True:
-                temp += c
-
-            else:
-                isInstruction = True
-
-
-        if isInstruction == True:
+        #I'll just deal with them the simple way lol, can't be bothered                            	                
+                     
+        if isInstruction(line.strip()) == True
             instruction_num += 1
-
-        if addLabel == True:
-
-            symboldict[temp] = str(instruction_num + 1)  # address of the next instruction, do we count comments in?
-                                                         # let's no store this in binary
+            
+        elif isLabel(line) == True:
+            label = getLabel(line.strip())
+            symboldict[label] = str(instruction_num + 1)# address of the next instruction, do we count comments in?
+   
+        else 
+            break                                                     # let's no store this in binary
+        
         linenum += 1
 
     f.close()
@@ -200,7 +162,7 @@ def checkInstructionA(line):
 def checkInstructionC(line):
     
     # I need to get rid of the  state/time dependency/spaghetti code
-    if (isLabel(line) == False) and (isComment(line) == False) and (checkInstructionA(line) == False) 
+    if (isLabel(line) == False) and (isNoise(line) == False) and (checkInstructionA(line) == False): 
     # the first instruction can't be a space(we passed in line.strip().
     	return True
 
@@ -263,8 +225,29 @@ def getTemp(line)
 
     return temp
 
-def getMachineCodeC(dest, comp, jump, parsejump):
+def getMachineCodeA(temp, variableAdress):
     
+    gotmachinecode = False  #I can try looping through these functions with a list
+
+    # check if temp is in the symbol table
+    machinecode, gotmachinecode = checkTempIfSymbol(temp)                     
+
+    # check if its a number then
+    if gotmachinecode == False:
+        machinecode, gotmachinecode = checkTempIfNumber(temp)
+                   
+    # it must be a variable then
+    if gotmachinecode == False:
+        machinecode, gotmachinecode, variableAdress = checkTempIfVariable(temp, variableAdress) 
+
+    return [machinecode, variableAdress]
+
+def getMachineCodeC(temp):
+        
+    dest = getdest(temp)
+    comp = getcomp(temp, dest)
+    jump, parsejump = getjump(temp) # parsejump is for addimg 000 in the machinecode if its False
+  
     # do an op-code
 
     machinecode = "111"   # unused bits?
@@ -321,6 +304,8 @@ def checkTempIfNumber(temp):
     return output.values()
 
 def checkTempIfVariable(temp, variableAdress):
+    
+    global symboldict
     
     output = {"machinecode": None, "gotmachinecode": None, "variableAdress": variableAdress}
     
@@ -408,74 +393,33 @@ def parse_file():    # so many things are inside on function....
 
     line = f.readline()
 
-    while line:  # if there are more lines to read?        
+    while line:          
 
-# what if there are like multiple instructions I won't be able to handle that
-# I feel like my code is so sloppy should like do this in C++ to know how bad my code is (it is efficient though...)
-
-        # how do I run each character
         print(line.strip())
-      
-        #replace temp with query
-        temp = getTemp(line)
+             
+        temp = getTemp(line.strip())   
 
-        # process the instruction
-        # this will only work it I don't allow spaces within instructions
+        if getInstructionType(line.strip()) == "A":
 
-# parse instruction and translate
-
-        #if toTranslate is not None: .....
-
-        if line.strip() is not "":     # why did not None not work?
-
-            if getInstructionType(line.strip()) == "A":
-
-                print("a instruction")
-
-                gotmachinecode = False
-
-                #I can try looping through these functions with a list
-                # check if temp is in the symbol table
-                machinecode, gotmachinecode = checkTempIfSymbol(temp)                     
-
-                # check if its a number then
-                if gotmachinecode == False:
-                    machinecode, gotmachinecode = checkTempIfNumber(temp)
-                   
-                # it must be a variable then
-                # How the hell do I fucking use encapsulation?
-                if gotmachinecode == False:
-                    machinecode, gotmachinecode, variableAdress = checkTempIfVariable(temp, variableAdress) 
-               
-            elif getInstructionType(line.strip()) == "C":
-
-                print("ey! C instruction!")                                                                                                                                                                                                                                                                                                        
-                        
-                dest = getdest(temp)
-                comp = getcomp(temp, dest)
-                jump, parsejump = getjump(temp) # parsejump is for addimg 000 in the machinecode if its False
-
-                machinecode = getMachineCodeC(dest, Icomp, jump, parsejump) 
-                       
-            #write the machine code onto the file
+            print("a instruction")
+            
+            machinecode, variableAdress = getMachineCodeA(temp, variableAdress)
+                           
+        elif getInstructionType(line.strip()) == "C":
+            
+            print("ey! C instruction!")                                                                                                                                                                                                                                                                                                        
+                                  
+            machinecode = getMachineCodeC(temp) 
+            
             #opening another file inside of a file, where should I put this though?
 
-            if (isComment(line.strip()) == False) and (isLabel(line.strip()) ==False):
-
-                # I know the problem, it still writes even though it is not an instruction
-
-                g.write(machinecode + "\n")
+        if (isNoise(line.strip()) == False) and (isLabel(line.strip()) ==False):                
+            g.write(machinecode + "\n")
 
         line = f.readline()
 
     f.close()
     g.close()
-
-
-# machine language specification:
-
-def code_generation():
-    pass
 
 if __name__ == "__main__":
 
